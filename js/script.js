d022f97921848031821e92730a956fe58caeb6d9
Daniele <danielelai86@gmail.com>
2021-02-26 16:06:59 +0100

    Aggiunti elementi html per stampare a video

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+// Chiedere il numero di chilometri e l'età
 var chilometri = prompt("Iniziamo il viaggio, quanti chilometri vuoi percorrere?");
 
 var eta = prompt("Quanti anni hai?");
 
+//Calcolare prezzo base del biglietto
 var prezzoBiglietto = 0.21 * chilometri;
 
+//Calcolare sconti
 var scontoGiovani = (20 * prezzoBiglietto) / 100;
 
 var scontoAnziani = (40 * prezzoBiglietto) / 100;
@@ -14,4 +17,4 @@ if (eta < 18) {
   prezzoBiglietto = prezzoBiglietto - scontoAnziani;
 }
 
-console.log(prezzoBiglietto);
+document.getElementById('prezzo_finale').innerHTML = "Il prezzo del tuo biglietto è: " + prezzoBiglietto + " euro"
